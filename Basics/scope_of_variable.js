597e07d30d624486affcc019a6288eb1f0a9318d
Luckyx100 <technicallucky861@gmail.com>
2023-09-14 19:27:23 +0530

    commit

--- Basics/scope_of_variable.js
+++ Basics/scope_of_variable.js
@@ -1,3 +1,9 @@
 //let name = "Lucky";  
-//let name = "Kumar";   --> Giving error
-//console.log(name);
+//let name = "Kumar";   --> Giving error, let can't be used for same variables
+//console.log(name);   // scope of let is only in the block of code (brackets) & can't be used outside the brackets
+
+var name = "Lucky"
+var name = "Kumar"   // var can be used anywhere in the code.....large scope
+console.log(name)
+
+// So unlike var, when you use let, a variable with the same name can only be declared once.
